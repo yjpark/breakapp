7ad868902e3f08bcdf37e0dee989e38a7961c339
YJ Park <yjpark@gmail.com>
2016-05-13 16:02:40 +0800

    build with ndk-r10e

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,5 @@
 APP_STL := stlport_static
 APP_ABI := armeabi armeabi-v7a
+
+# https://groups.google.com/forum/#!topic/google-breakpad-discuss/a8yfEil71ic
+APP_CPPFLAGS := -std=c++11 -D__STDC_LIMIT_MACROS
